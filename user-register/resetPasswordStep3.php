eaa228345253f3e152c9c61b858364898b5ccb01
Chaoran Wang <superchaoran@gmail.com>
2016-04-28 15:49:19 -0400

    fix password empty problem

--- user-register/resetPasswordStep3.php
+++ user-register/resetPasswordStep3.php
@@ -7,8 +7,12 @@ $email_token = htmlentities($_POST["token"]);
 $email = htmlentities($_POST["email"]);
 $newPassword = htmlentities($_POST["newPassword"]);
 $confirmPassword = htmlentities($_POST["confirmPassword"]);
+if(empty($newPassword) || empty($confirmPassword)){
+echo '<h1>Password cannot be empty!</h1>';
+header('Refresh: 1; url = '. $_SERVER['HTTP_REFERER']);
+}
 if(strcmp($newPassword,$confirmPassword) !== 0){
-echo '<h1>Pasword mismatch!</h1>';
+echo '<h1>Password mismatch!</h1>';
 header('Refresh: 1; url = '. $_SERVER['HTTP_REFERER']);
 } else {
 $secure_password = md5($newPassword);
