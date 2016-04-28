311fca11f4e911e5d72a0d01fdb7e61d0ebcd020
Chaoran Wang <superchaoran@gmail.com>
2016-04-28 11:38:24 -0400

    Fix bug call uninstantiated class function need self:: ;extract out mailing function

--- user-register/Classes/EmailConfirmation.php
+++ user-register/Classes/EmailConfirmation.php
@@ -12,6 +12,44 @@ class EmailConfirmation {
 		return $randomString;
 	}
 
+
+
+        function sendEmail($messageDetails) {	
+		$from = $messageDetails["from_name"] . " <". $messageDetails["from_email"]. ">" . "\r\n";
+		$to = $messageDetails["to_email"];
+		$subject = $messageDetails["message_subject"];
+		$body = $messageDetails["message_body"];
+ 		$headers = array ('From' => $from, 'To' => $to, 'Subject' => $subject);
+	
+		$host = "smtp.gmail.com";
+		$username = "food@jogchat.com";
+		$password = "8515111q";
+
+	
+		$crlf = "\n";
+		$mime = new Mail_mime(array('eol' => $crlf));
+		//$mime->setTXTBody('Please click following link to activate your account: '.$tokenLink);
+		$mime->setHTMLBody($body);
+		$headers = $mime->headers($headers);
+		$body = $mime->get();
+
+		$smtp = Mail::factory('smtp', array ('host' => $host, 'auth' => true, 'username' => $username, 'password' => $password));
+		$mail = $smtp->send($to, $headers, $body);
+		/*		
+		$headers = array ('From' => $from, 'To' => $to, 'Subject' => $subject);
+		$smtp = Mail::factory('smtp', array ('host' => $host, 'auth' => true, 'username' => $username, 'password' => $password));
+		$mail = $smtp->send($to, $headers, $body);
+		*/	
+		/*
+		$headers = "From: food@jogchat.com\r\n";
+		$headers .= "Reply-To: ". $from;
+		$headers .= "Return-Path: ". $from;
+    		$headers .= "X-Mailer: PHP/" . phpversion()."\r\n";
+    		$headers .= "MIME-Version: 1.0\r\n";
+    		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";  
+		$returnValue = mail($to, $subject, $messageDetails["message_body"], $headers);
+		*/	
+	}
 	function sendEmailConfirmation($email, $emailToken) {
 
 		$randomAdd = '';
@@ -32,49 +70,41 @@ class EmailConfirmation {
 		<html>
 		  <body>
 		    <h1>'.$randomAdd.' Welcome to Jogchat.com '.$randomAdd.'</h1>
-		    <h1>Please click on the following link to activate your account: </h1>'.
-		   '<h1>'.$randomAdd.'<a href ="'.$tokenLink.'">link</a>'.$randomAdd.'</h1>
+		    <h1>Please click on the following link to activate your account: </h1>
+		    <h1>'.$randomAdd.'<a href ="'.$tokenLink.'">link</a>'.$randomAdd.'</h1>
 		  </body>
 		</html>
 		';
+		self::sendEmail($messageDetails);
+	 }
+
+	 function sendEmailPasswordReset($email,$emailToken) {
+		$randomAdd = ''; 
+                $num = rand(1, 10);
+                for ($x = 0; $x <= $num; $x++) {
+                 $randomAdd = $randomAdd.'*';
+                }   
+
+                $messageDetails = array();
+                $messageDetails["message_subject"] = $randomAdd."Reset Your Jogchat Account Password".$randomAdd;
+                $messageDetails["to_email"] = $email;
+                $messageDetails["from_name"] = "Food Jogchat";
+                $messageDetails["from_email"] = "food@jogchat.com";
+
+                $resetLink = 'http://www.jogchat.com/Food-Order-Entrepreneur-PHP/user-register/resetPasswordStep2.php?token='.$emailToken.'&email='.$email;
+                $messageDetails["message_body"] ='  
+                <html>
+                  <body>
+                    <h1>'.$randomAdd.' Welcome to Jogchat.com '.$randomAdd.'</h1>
+                    <h1>Please click on the following link to reset your password: </h1>
+                    <h1>'.$randomAdd.'<a href ="'.$resetLink.'">link</a>'.$randomAdd.'</h1>
+                  </body>
+                </html>
+                ';
+                self::sendEmail($messageDetails);		
+	 }
 
 
-		
-		$from = $messageDetails["from_name"] . " <". $messageDetails["from_email"]. ">" . "\r\n";
-		$to = $messageDetails["to_email"];
-		$subject = $messageDetails["message_subject"];
-		$body = $messageDetails["message_body"];
- 		$headers = array ('From' => $from, 'To' => $to, 'Subject' => $subject);
-	
-		$host = "smtp.gmail.com";
-		$username = "food@jogchat.com";
-		$password = "8515111q";
-	
-		$crlf = "\n";
-		$mime = new Mail_mime(array('eol' => $crlf));
-		//$mime->setTXTBody('Please click following link to activate your account: '.$tokenLink);
-		$mime->setHTMLBody($body);
-		$headers = $mime->headers($headers);
-		$body = $mime->get();
-
-		$smtp = Mail::factory('smtp',
-		array ('host' => $host, 'auth' => true, 'username' => $username, 'password' => $password));
-		$mail = $smtp->send($to, $headers, $body);
-		/*		
-		$headers = array ('From' => $from, 'To' => $to, 'Subject' => $subject);
-		$smtp = Mail::factory('smtp', array ('host' => $host, 'auth' => true, 'username' => $username, 'password' => $password));
-		$mail = $smtp->send($to, $headers, $body);
-		*/	
-		/*
-		$headers = "From: food@jogchat.com\r\n";
-		$headers .= "Reply-To: ". $from;
-		$headers .= "Return-Path: ". $from;
-    		$headers .= "X-Mailer: PHP/" . phpversion()."\r\n";
-    		$headers .= "MIME-Version: 1.0\r\n";
-    		$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";  
-		$returnValue = mail($to, $subject, $messageDetails["message_body"], $headers);
-		*/
-	}
 }
 ?>
 
